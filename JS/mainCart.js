5d3556045f20ac6234e68241d9c189dbb9fd85f3
Nicolas Plante <plante.nicolas.4@courrier.uqam.ca>
2022-03-20 12:45:28 -0400

    Terminer afficher/cacher cart

--- JS/mainCart.js
+++ JS/mainCart.js
@@ -1,30 +1,7 @@
-// // cart
-// let cartIcon = document.querySelector("#cart-icon");
-// let cart = document.querySelector(".cart");
-// let closeCart = document.querySelector("#close-cart");
+$('#cart-icon').click(function(){
+  $('.cart').addClass('active');
+});
 
-// //open cart
-// cartIcon.onlclick = () => {
-//   cart.classList.add("active");
-// };
-
-// //close cart
-// closeCart.onlclick = () => {
-//   cart.classList.remove("active");
-// };
-
-function open() {
-  document.querySelector(".cart").classList.add("active");
-}
-
-function close() {
-  document.querySelector(".cart").classList.remove("active");
-}
-
-document.querySelector("#cart-icon").onlclick = function () {
-  open();
-};
-
-document.querySelector("#close-cart").onlclick = function () {
-  close();
-};
+$('#close-cart').click(function(){
+  $('.cart').removeClass('active');
+});
