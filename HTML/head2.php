5b324ad1702cbeb86f98f4178fe56a5f55cd4fcf
oussamaelfig <oelfigha2002@gmail.com>
2022-03-28 14:32:04 -0400

    ajout de tout les photos et pages de description de produit

--- HTML/head2.php
+++ HTML/head2.php
@@ -1,37 +1,37 @@
 <!-- navbar -->
 <nav class="navbar navbar-expand-lg py-3 sticky-top navbar-light bg-light">
     <div class="container">
-      <a class="navbar-brand" href="../index.php"><img id="logo-comp" class="logo img-fluid" src="../Images/logoEnt1.png" alt="logo" /></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="../index.php">Accueil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="explorer.php">Explorer</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.php">À propos</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="postuler.php">Postuler</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="signIn.php">Se connecter</a>
-          </li>
-        </ul>
-        <div class="d-flex align-items-center">
-      <!-- Icon -->
-      <button type="button" class="btn btn-light" id="open-cart">
-              <a class="text-reset me-3" href="cart.php">
-                <i class="fas fa-shopping-cart" id="cart-icon"></i> </a
-              ><sup class="cartCount">0</sup>
-            </button>
+        <a class="navbar-brand" href="../index.php"><img id="logo-comp" class="logo img-fluid"
+                src="../Images/logoEnt1.png" alt="logo" /></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="../index.php">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="explorer.php">Explorer</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">À propos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="postuler.php">Postuler</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="signIn.php">Se connecter</a>
+                </li>
+            </ul>
+            <div class="d-flex align-items-center">
+                <!-- Icon -->
+                <button type="button" class="btn btn-light" id="open-cart">
+                    <a class="text-reset me-3" href="cart.php">
+                        <i class="fas fa-shopping-cart" id="cart-icon"></i> </a><sup class="cartCount">0</sup>
+                </button>
+            </div>
         </div>
-      </div>
     </div>
-  </nav>
+</nav>
