32ca197726a8ba0f9f8f0d545bd61ff547cbf4d3
oussamaelfig <oelfigha2002@gmail.com>
2022-04-18 00:22:42 -0400

    ajout du code panier

---
+++ vpi-app/src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent implements OnInit {
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {}
+
+  itemCount() {
+    return this.cartService.itemsCount();
+  }
+}
