e7c3201f5dc780334f30516e2fdab1d5d9d74501
Nicolas Plante <plante.nicolas.4@courrier.uqam.ca>
2022-04-19 19:55:25 -0400

    ajout méthode de validation

--- vpi-app/src/app/postuler/postuler.component.ts
+++ vpi-app/src/app/postuler/postuler.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 
 @Component({
@@ -19,4 +18,34 @@ export class PostulerComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  private validerNom(){
+    if(this.nom.value == '' || /\d/.test(this.nom.value)){
+      alert("Le nom est invalide");
+      return false;
+    }
+    return true
+  }
+
+  private validerPrenom(){
+    if(this.prenom.value == '' || /\d/.test(this.prenom.value)){
+      alert("Le prénom est invalide");
+      return false;
+    }
+    return true
+  }
+
+  private validerDate(){
+    if(this.date.value == ''){
+      alert("La date de naissance est obligatoire");
+      return false;
+    }
+    return true;
+  }
+
+  validerPostuler(){
+    this.validerNom();
+    this.validerPrenom();
+    this.validerDate();
+    //If all vrai -> ecrire dans json
+  }
 }
