f0e0d437f8d8fb38ce18fee8b77a4d361db8d842
oussamaelfig <oelfigha2002@gmail.com>
2022-04-25 16:19:42 -0400

    regler conflit login

--- .history/vpi-app/src/app/login/login.component_20220425161913.ts
+++ .history/vpi-app/src/app/login/login.component_20220425161913.ts
@@ -11,9 +11,9 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   identifiant = new FormControl('');
   motDePasse = new FormControl('');
-  private utilisS: string = 'utilisateur';
+  private utilisS:string = "utilisateur";
 
-  constructor(private bd: BdService, private router: Router) {}
+  constructor(private bd: BdService, private router: Router) { }
 
   ngOnInit(): void {}
 
@@ -33,6 +33,8 @@ export class LoginComponent implements OnInit {
     console.log(this.users.body);
   }
 
+
+
   //méthode pour ce connecter en verifiant les infos entrees par user
   //avec celle qui ce trouve dans le json sur le serveur tpapp au port 3001
   validerLogin() {
@@ -44,6 +46,7 @@ export class LoginComponent implements OnInit {
       let cpt = 0;
       console.log(this.users);
       //regarder avec si correspond entre les users[i].username -> si oui, on regarde le role et on crée un cookie avec valeur role. else -> return false
+<<<<<<< HEAD
       while (cpt < this.users.length) {
         if (
           this.identifiant.value == this.users[cpt].username &&
@@ -51,12 +54,27 @@ export class LoginComponent implements OnInit {
         ) {
           console.log(this.users);
           if (this.users[cpt].role == 'user') {
-            localStorage.setItem(this.utilisS, 'user');
+            localStorage.setItem('utilisateur', 'user');
           } else if (this.users[cpt].role == 'admin') {
-            localStorage.setItem(this.utilisS, 'admin');
+            localStorage.setItem('utilisateur', 'admin');
           }
-          this.router.navigateByUrl('/succes');
+          //set cookie avec le role
+          //renvoyer vers la nouvelle page.
+          alert('you good');
           return true;
+=======
+      while(cpt < this.users.length){
+        if(this.identifiant.value == this.users[cpt].username &&
+          this.motDePasse.value == this.users[cpt].motdepasse){
+            console.log(this.users);
+            if(this.users[cpt].role == "user"){
+              localStorage.setItem(this.utilisS, "user");
+            }else if(this.users[cpt].role == "admin"){
+              localStorage.setItem(this.utilisS, "admin")
+            }
+            this.router.navigateByUrl('/succes');
+            return true;
+>>>>>>> 0ca1c7c993e9fa59678a07091b86983ae9d1f47b
         }
         ++cpt;
       }
