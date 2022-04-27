711229dda76c3a273e4cb20025484d0de194d177
Nicolas Plante <plante.nicolas.4@courrier.uqam.ca>
2022-04-26 22:47:02 -0400

    modif admin non fonctionnel

--- vpi-app/src/app/admin/admin.component.ts
+++ vpi-app/src/app/admin/admin.component.ts
@@ -1,11 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 import { BdService } from '../bd.service';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import {AfterViewInit, ViewChild} from '@angular/core';
 
 @Component({
@@ -13,42 +11,50 @@ import {AfterViewInit, ViewChild} from '@angular/core';
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.css']
 })
-export class AdminComponent implements AfterViewInit{
+export class AdminComponent implements OnInit{
+  //DataSource
+  dataSource:any = this.bd.getUser().subscribe((res) => {
+    this.dataSource = res.body;
+    console.log(this.dataSource);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  });
   //Inititalisation de users
   users: any = this.bd.getUser().subscribe((res) => {
-    this.users = res.body;
-    console.log(this.users);
+    this.users = res;
+    console.log(this.dataSource);
   });
+
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
   //FormControl variable pour creation usager
   nom = new FormControl('');
   prenom = new FormControl('');
   motDePasse = new FormControl('');
   identifiant = new FormControl('');
   role = new FormControl('');
-  dataSource:any;
+
+
 
   constructor(private bd:BdService) {}
 
   //Pour material table
   colonneAfficher:string[] = ['id', 'username', 'motdepasse', 'nom', 'prenom', 'role'];
 
-  @ViewChild(MatPaginator) paginator: MatPaginator|undefined;
 
-  ngAfterViewInit() {
-    this.dataSource = new MatTableDataSource<usager>(this.users);
-    this.dataSource.paginator = this.paginator;
-  }
 
+  ngOnInit(){}
   //fonction creer usager
 
   //fonction lister usager
 
   //table material pour lister et supprimer usager.
 }
-export interface usager {
-  id: number;
+export interface Usager {
   identifiant: string;
   motdepasse: string;
+  id: number;
   nom: string;
   prenom: string;
   role: string;
