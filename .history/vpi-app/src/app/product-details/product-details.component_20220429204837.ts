dda15d5ee9f6f3ceef56b8d15d74b8d1e58d3213
oussamaelfig <oelfigha2002@gmail.com>
2022-04-29 21:13:35 -0400

    appel de la get method

---
+++ .history/vpi-app/src/app/product-details/product-details.component_20220429204837.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Product, products } from '../products';
+import { BdService } from '../bd.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  //product: Product | undefined;
+  items = this.bdService.getItems();
+
+  dataSource: any;
+
+  observer2: any = this.bdService.getProduits().subscribe((res) => {
+    this.dataSource = res.body;
+    console.log(this.dataSource);
+  });
+
+  constructor(private route: ActivatedRoute, private bdService: BdService) {}
+
+  ngOnInit(): void {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Find the product that correspond with the id provided in route.
+    this.dataSource = products.find(
+      (product: { id: number }) => product.id === productIdFromRoute
+    );
+  }
+
+  addToCart(product: any) {
+    this.bdService.togglePanier(product);
+    localStorage.setItem('products', JSON.stringify(this.items));
+  }
+}
+
+@Pipe({ name: 'safe' })
+export class SafePipe implements PipeTransform {
+  constructor(private domSanitizer: DomSanitizer) {}
+  transform(url: string) {
+    return this.domSanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
